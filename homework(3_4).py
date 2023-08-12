94c01e6e2853744939334439049e2fbb91a38f01
Markelo <vologdin.mark@gmail.com>
2023-08-12 10:07:44 -0700

    first commit

--- homework(3_4).py
+++ homework(3_4).py
@@ -25,4 +25,8 @@ def factorial():
     return fact
  #3628800
 if __name__ == '__main__':
-    print(factorial())
+    n = int(input())
+    a = [0]*n
+    for i in range(n):
+        a[i] = int(input())
+    print(a)
